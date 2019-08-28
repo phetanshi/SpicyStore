00c54ccfcebb257ae846c929b99e3d0b39761a5c
phetanshi <padmasekhar.pottepalem@gmail.com>
2019-08-28 21:18:24 +0530

    First Commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom';
+import logo from '../logo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
+import styled from 'styled-components';
+import {ButtonContainer} from './Button';
+
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
+                <Link to="/">
+                    <img src={logo} alt="Store" className="navbar-brand" />
+                </Link>
+                <ul className="navbar-nav align-items-center">
+                    <li className="nav-item ml-5">
+                        <Link to="/" className="nav-link">
+                            products
+                        </Link>
+                    </li>
+                </ul>
+                <Link to="/cart" className="ml-auto">
+                    <ButtonContainer>
+                        <span className="mr-2">
+                            <FontAwesomeIcon  icon={faCartPlus} />
+                        </span>
+                        My Cart
+                    </ButtonContainer>
+                </Link>
+            </NavWrapper>
+        )
+    }
+}
+
+const NavWrapper = styled.nav`
+background:var(--mainBlue);
+.nav-link{
+    color:var(--mainWhite) !important;
+    font-size:1.3rem;
+    text-transform:capitalize;
+}
+`;
